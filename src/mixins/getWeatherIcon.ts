623e4997aa1d3f7a017fab6f48caad31b7c951e2
keisuke watanabe <kskwtnb93@gmail.com>
2023-03-22 01:28:52 +0900

    fix: アイコンのコードが取得出来ないときのコンソールエラー対策で、アイコン画像パス取得関数を修正

--- src/mixins/getWeatherIcon.ts
+++ src/mixins/getWeatherIcon.ts
@@ -1,5 +1,7 @@
 // 天気のアイコンの画像パスを生成する処理
 export const getWeatherIconPath = (code: string, size = '@4x') => {
-  const iconUrl = 'https://openweathermap.org/img/wn/' + code + size + '.png'
-  return iconUrl
+  if (code) {
+    const iconUrl = 'https://openweathermap.org/img/wn/' + code + size + '.png'
+    return iconUrl
+  }
 }
